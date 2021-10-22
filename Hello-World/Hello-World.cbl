966b8a3b17ffc04d3a2f02a303f4ab814b2359e7
Wagner Queiroz <wagnersq@gmail.com>
2021-10-22 08:45:40 -0300

    Getting started with COBOL

---
+++ Hello-World/Hello-World.cbl
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+           PROGRAM-ID. Hello-World.
+           AUTHOR. "wagnersq@gmail.com".
+
+       DATA DIVISION.
+
+       FILE SECTION.
+
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+           MAIN-PROCEDURE.
+               DISPLAY "Hello world" END-DISPLAY.
+               STOP RUN.
+               GOBACK.
+           END PROGRAM Hello-World.
